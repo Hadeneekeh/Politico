ab227cd583f1fd863aabeac0b82e5671c7225684
Kafilat Abdulwahab <hadeneekeh01@gmail.com>
2019-02-01 12:12:08 +0100

    feat-create-party-ensure-that-an-admin-can-create-party-[delivers

---
+++ Server/db/db.js
@@ -0,0 +1,11 @@
+const parties =  [
+    {
+      id: 1,
+      name: "The First Party",
+      hqAddress: "Wuse, Abuja",
+      logoUrl: "http://something.com"
+     
+    }
+];
+
+export default parties;
